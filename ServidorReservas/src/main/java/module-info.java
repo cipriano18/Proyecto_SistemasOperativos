6a99867c89c8fe33e8cbb54015410123eb150f5c
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2026-03-17 20:38:24 -0600

    delete ServidorReservas

--- ServidorReservas/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module proyect.servidorreservas {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens proyect.servidorreservas to javafx.fxml;
-    exports proyect.servidorreservas;
-}
