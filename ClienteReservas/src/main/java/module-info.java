46c9fdb42f063ef6fd7ba34e2f003b1573a0d68a
MakartzZz <makin.artavia.zuniga@est.una.ac.cr>
2026-03-21 20:53:47 -0600

    Funcional
    El proyecto corre correctamente

--- ClienteReservas/src/main/java/module-info.java
+++ ClienteReservas/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module proyect.clientereservas {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires transitive javafx.graphics;
+    
     opens proyect.clientereservas to javafx.fxml;
     exports proyect.clientereservas;
 }
