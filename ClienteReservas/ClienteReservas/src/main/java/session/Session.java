f4089fd5a8c2fb69b0945e9949f7d315a610e56e
lolkingjra <ja2159586@gmail.com>
2026-04-20 22:09:09 -0600

    añadiendo session para perfil
     - Guarda el ClientRequest del usuario logueado en una instancia única accesible desde cualquier controller.
        - Tiene setClient(...) (llamado desde el login tras éxito), getClient() (llamado desde el perfil y home), clear() (para logout o tras eliminar la
        cuenta) y isLoggedIn() (defensa para que no se pueda abrir el perfil sin sesión).

---
+++ ClienteReservas/ClienteReservas/src/main/java/session/Session.java
@@ -0,0 +1,35 @@
+package session;
+
+import model.ClientRequest;
+
+public class Session {
+
+    private static Session instance;
+    private ClientRequest client;
+
+    private Session() {
+    }
+
+    public static synchronized Session getInstance() {
+        if (instance == null) {
+            instance = new Session();
+        }
+        return instance;
+    }
+
+    public ClientRequest getClient() {
+        return client;
+    }
+
+    public void setClient(ClientRequest client) {
+        this.client = client;
+    }
+
+    public void clear() {
+        this.client = null;
+    }
+
+    public boolean isLoggedIn() {
+        return client != null;
+    }
+}
